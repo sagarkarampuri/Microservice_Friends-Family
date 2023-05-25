ae68859d6ae5137d9bc852cfcc9a907d72ea4ae3
sagarkarampuri <karampurisagar3@gmail.com>
2023-05-25 19:06:01 +0530

    first commit

---
+++ src/main/java/com/ms/ffd/MicroserviceFriendsFamilyApplication.java
@@ -0,0 +1,13 @@
+package com.ms.ffd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceFriendsFamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceFriendsFamilyApplication.class, args);
+	}
+
+}
